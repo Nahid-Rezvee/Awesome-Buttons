012578726b08fba5910ab89ce54925be1ab6f411
Nahid <abdun.nahid@gmail.com>
2019-05-24 17:10:39 +0600

    navbar sidemenu dropdown added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var mtrlButtons = document.querySelectorAll(".showcase .mtrl-btn");
 var modal = document.querySelector(".snippet-modal-background");
+var dropdown = document.querySelector(".dropdown");
+var sidemenu = document.querySelector(".sidemenu");
+var showcase = document.querySelector(".showcase");
 var copiedTooltip = document.querySelector(".copied-tooltip");
 var codeSnippet = document.querySelector("#code-snippet");
 
 isToolTipActive = false;
+isSideMenuActive = false;
+isDropdownActive = false;
 
 function showCode(e) {
     const rx = new RegExp("<span[\\d\\D]*?\/span>", "g");
@@ -26,6 +31,49 @@ function showModal() {
     modal.classList.add('show');
 }
 
+function hideModal() {
+    modal.classList.remove('show');
+}
+
+function toggleSidemenu() {
+    if (isSideMenuActive) {
+        hideSidemenu();
+    } else {
+        showSidemenu();
+    }
+}
+
+function showSidemenu() {
+    sidemenu.classList.add('show');
+    showcase.classList.add('shrink');
+    isSideMenuActive = true;
+}
+
+function hideSidemenu() {
+    sidemenu.classList.remove('show');
+    showcase.classList.remove('shrink');
+    isSideMenuActive = false;
+}
+
+
+function toggleDropdown() {
+    if (isDropdownActive) {
+        hideDropdown();
+    } else {
+        showDropdown();
+    }
+}
+
+function showDropdown() {
+    dropdown.classList.add('show');
+    isDropdownActive = true;
+}
+
+function hideDropdown() {
+    dropdown.classList.remove('show');
+    isDropdownActive = false;
+}
+
 function showToolTip() {
     if (!isToolTipActive) {
         copiedTooltip.classList.add('show-tooltip');
@@ -35,10 +83,6 @@ function showToolTip() {
             isToolTipActive = false;
         }, 1000);
     }
-}
-
-function hideModal() {
-    modal.classList.remove('show');
 }
 
 function clearSelection() {
