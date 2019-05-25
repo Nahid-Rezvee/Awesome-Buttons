3d7b54cb5c0d9301228c8b605df6678a7cd83a2b
Nahid <abdun.nahid@gmail.com>
2019-05-25 21:46:49 +0600

    theme settings done

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var mtrlButtons = document.querySelectorAll(".showcase .mtrl-btn");
 var modal = document.querySelector(".snippet-modal-background");
 var dropdown = document.querySelector(".dropdown");
+var settings = document.querySelector(".settings");
 var sidemenu = document.querySelector(".sidemenu");
 var showcase = document.querySelector(".showcase");
 var copiedTooltip = document.querySelector(".copied-tooltip");
 var codeSnippet = document.querySelector("#code-snippet");
 
+// Theme Variables
+var primaryColorInputDOM = document.querySelector("#primary");
+var accentColorInputDOM = document.querySelector("#accent");
+var warningColorInputDOM = document.querySelector("#warning");
+var dangerColorInputDOM = document.querySelector("#danger");
+var darkColorInputDOM = document.querySelector("#dark");
+var lightColorInputDOM = document.querySelector("#light");
+var settingsInputDoms = document.querySelectorAll(".settings input");
+
 isToolTipActive = false;
 isSideMenuActive = false;
 isDropdownActive = false;
+isSettingsActive = false;
 
 function showCode(e) {
     const rx = new RegExp("<span[\\d\\D]*?\/span>", "g");
@@ -18,7 +29,18 @@ function showCode(e) {
     showModal();
 }
 
+updateTheme();
+
+settingsInputDoms.forEach(input => {
+    input.addEventListener('keydown', function onEvent(event) {
+        if (event.key === "Enter") {
+            updateTheme();
+        }
+    });
+});
+
 mtrlButtons.forEach(link => link.addEventListener("click", showCode));
+
 
 function copyCode() {
     codeSnippet.select();
@@ -55,7 +77,6 @@ function hideSidemenu() {
     isSideMenuActive = false;
 }
 
-
 function toggleDropdown() {
     if (isDropdownActive) {
         hideDropdown();
@@ -72,6 +93,24 @@ function showDropdown() {
 function hideDropdown() {
     dropdown.classList.remove('show');
     isDropdownActive = false;
+}
+
+function toggleSettings() {
+    if (isSettingsActive) {
+        hideSettings();
+    } else {
+        showSettings();
+    }
+}
+
+function showSettings() {
+    settings.classList.add('show');
+    isSettingsActive = true;
+}
+
+function hideSettings() {
+    settings.classList.remove('show');
+    isSettingsActive = false;
 }
 
 function showToolTip() {
@@ -95,4 +134,32 @@ function clearSelection() {
     } else if (document.selection) { // IE?
         document.selection.empty();
     }
+}
+
+function updateTheme() {
+    const primaryColor = primaryColorInputDOM.value;
+    const accentColor = accentColorInputDOM.value;
+    const warningColor = warningColorInputDOM.value;
+    const dangerColor = dangerColorInputDOM.value;
+    const darkColor = darkColorInputDOM.value;
+    const lightColor = lightColorInputDOM.value;
+
+    primaryColorInputDOM.parentElement.style.borderBottomColor = primaryColor;
+    accentColorInputDOM.parentElement.style.borderBottomColor = accentColor;
+    warningColorInputDOM.parentElement.style.borderBottomColor = warningColor;
+    dangerColorInputDOM.parentElement.style.borderBottomColor = dangerColor;
+    darkColorInputDOM.parentElement.style.borderBottomColor = darkColor;
+    lightColorInputDOM.parentElement.style.borderBottomColor = lightColor;
+
+
+    mtrlButtons.forEach(btn => {
+        btn.style.setProperty('--primary-color', primaryColor);
+        btn.style.setProperty('--accent-color', accentColor);
+        btn.style.setProperty('--warning-color', warningColor);
+        btn.style.setProperty('--danger-color', dangerColor);
+        btn.style.setProperty('--dark-color', darkColor);
+        btn.style.setProperty('--dark-color', darkColor);
+        btn.style.setProperty('--light-color', lightColor);
+    });
+
 }
